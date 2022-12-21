de9277ee3c4640bab9d270df85761152fe329807
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-21 18:09:50 +0300

    Tasks

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -32,5 +32,5 @@ for (int i = 0; i < n; i++)
                 {
                     a += array[t, i];
                 }
-                Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {(float) a / m}");
+                Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {Math.Round((double) a / m , 2)}");
             }
